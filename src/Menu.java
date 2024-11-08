910cec2d1b03b58f917ed6af61c553b31cf20655
Edisonfeh <monedis.10@hotmail.com>
2024-11-08 16:14:16 -0500

    actualiza readme

--- src/Menu.java
+++ src/Menu.java
@@ -2,6 +2,6 @@ public class Menu {
 
     public void metodo1(){
         System.out.println("llamada del metodo a prueba");
-        System.out.println("prueba de conexion exitosa");
+        System.out.println("prueba de conexion no existosa ahora?");
     }
 }
